f93e9cbad6480b62b37d4f0cb447e1aa6648eff9
viny1ic <vinayak.dubey2019@vitstudent.ac.in>
2021-09-27 23:08:08 +0530

    request handler added

---
+++ signin.php
@@ -0,0 +1,13 @@
+<?php
+    $TOKEN = "abcdefghi"
+    $name = $_GET["name"];
+    $ticket = $_GET["ticket"];
+    $mob = $_GET["mob"]
+    $token = $_GET["token"]
+    #DB interface to query the token to be implemented
+    if ($TOKEN != $token){
+        echo "something went wrong"
+    }
+    if ($TOKEN == $token){
+        echo "authenticated! enjoy your high speed internet connection"
+    }
